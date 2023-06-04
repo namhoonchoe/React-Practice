73f4439689deb06a5f1602550a89c4571bb6be7d
Namhoon Choe <sysynh2012@gmail.com>
2023-06-04 12:14:47 +0900

    alias setups
    절대경로 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Blabla from '@components/Blabla'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,9 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Blabla/>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
---
+++ src/components/Blabla.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Blabla = () => {
+  return (
+    <div>blabla</div>
+  )
+}
+
+export default Blabla
